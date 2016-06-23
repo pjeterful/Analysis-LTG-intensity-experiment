35366632d06ecb2f1337372f804951dff69f24bf
pjeterful <pieterstobbe@hotmail.com>
2016-06-23 15:06:12 +0200

    Fixed some the annotations

--- ISI.m
+++ ISI.m
@@ -1,5 +1,12 @@
 function ISI_plot=ISI(analysis_ctrl, analysis_LTG, which_cells, which_traces, window)
 
+% This function calculates the times between two spikes following a single
+% laser pulse (doublet) when it occures. It plots these times in a
+% histogram and in a graph per  stimulus number. Input is a analysis_ctrl
+% and LTG struct, which cells and traces are of interest and the time window
+% whithin which the spikes must follow the laser pulse to be counted as
+% valid. PAS 23-06-2016.
+
 % Plot in a new figure for each new cell
 for cell=[which_cells];
     figure(cell);
@@ -9,10 +16,9 @@ for cell=[which_cells];
     for sweep=[which_traces];
         c=c+1;       
         subplot(nr_sw,2,c); 
-            %Calcuate the post stimulus times of all detected laser peaks for
-            % the control conditions. Copy the laser times from the analysis struct
+            % Copy the laser times from the analysis struct
             lasertimes_ctrl=analysis_ctrl{1,cell}(sweep).laser_peaks;
-             % Copy the spike times per repeat for ev]ery repeat
+             % Copy the spike times per repeat for every repeat
              for repeat=1:size(analysis_ctrl{1, cell}(sweep).spike_peaks,2);
                  spiketimes_ctrl{1,repeat}=analysis_ctrl{1,cell}(sweep).spike_peaks{1,repeat};
                 % The number of laser stimulations 
@@ -21,42 +27,42 @@ for cell=[which_cells];
                      SAL_ctrl=find(spiketimes_ctrl{1,repeat} > lasertimes_ctrl(stim)...
                       & spiketimes_ctrl{1,repeat} < (lasertimes_ctrl(stim) + window));
                     % When no spike is found within the the window, find
-                    % returns an empty vector, replace the empty cell
-                    % with a 0.
+                    % returns an empty vector, replace the empty cell. When
+                    %there are no doublets following a laser pulse (so less
+                    %then 2 spike within the window) place a NaN en continue 
+                    %to the next laser stimulus.
                   
                      if size(SAL_ctrl, 1)<2
                         ISI_ctrl{1,repeat}(stim,:)=NaN;
                          continue
+                         %Else take the first to spike times (doublet) of this stimulus 
+                         %and put them  in the variable
                      else
                          SAL_ctrl(1)=spiketimes_ctrl{1,repeat}(SAL_ctrl(1));
                          SAL_ctrl(2)=spiketimes_ctrl{1,repeat}(SAL_ctrl(2));
                      end
-                     
-                     % If the cell containes a zero put a NaN in the new
-                     % struct, if not, substract the laser time from the
-                     % corresponding spike time
-                 
+                        %Calculate the time between these spikes (ISI) by
+                        %suprtracting one from the other. 
                       ISI_ctrl{1,repeat}(stim,:)=SAL_ctrl(2)-SAL_ctrl(1);
                  end
              end
+             %Combine the repeats in vecors and a matrix individualy so
+             %they can be easely plotted in a histogram and regular plot
+             %sepperately.
              comb_ISI_ctrl=[ISI_ctrl{1,1};ISI_ctrl{1,2};ISI_ctrl{1,3}];  
              c_ISI_ctrl=[ISI_ctrl{1,1},ISI_ctrl{1,2},ISI_ctrl{1,3}];
   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- %Calcuate the post stimulus times of all detected laser peaks for
-            % the LTG conditions. Copy the laser times from the analysis struct
+%DO the same thing for the LTG condition.
             lasertimes_LTG=analysis_LTG{1,cell}(sweep).laser_peaks;
              % Copy the spike times per repeat for ev]ery repeat
-             for repeat=1:size(analysis_LTG{1, cell}(sweep).spike_peaks,2);
+             for repeat=1:size(analysis_LTG{1, cell}(sweep).spike_p.eaks,2);
                  spiketimes_LTG{1,repeat}=analysis_LTG{1,cell}(sweep).spike_peaks{1,repeat};
-                % The number of laser stimulations 
+                % The number of laser stimulations .
                  for stim=1:size(lasertimes_LTG,1);
                      % Find every spike within the spike window 
                      SAL_LTG=find(spiketimes_LTG{1,repeat} > lasertimes_LTG(stim)...
-                      & spiketimes_LTG{1,repeat} < (lasertimes_LTG(stim) + window));
-                    % When no spike is found within the the window, find
-                    % returns an empty vector, replace the empty cell
-                    % with a 0.
+                      & spiketimes_LTG{1,repeat} < (lasertimes_LTG(stim) + window))
                   
                        if size(SAL_LTG, 1)<2
                         ISI_LTG{1,repeat}(stim,:)=NaN;
@@ -65,10 +71,6 @@ for cell=[which_cells];
                          SAL_LTG(1)=spiketimes_LTG{1,repeat}(SAL_LTG(1));
                          SAL_LTG(2)=spiketimes_LTG{1,repeat}(SAL_LTG(2));
                      end
-                     
-                     % If the cell containes a zero put a NaN in the new
-                     % struct, if not, substract the laser time from the
-                     % corresponding spike time
                    
                       ISI_LTG{1,repeat}(stim,:)=SAL_LTG(2)-SAL_LTG(1);   
                  end
@@ -77,7 +79,7 @@ for cell=[which_cells];
               c_ISI_LTG=[ISI_LTG{1,1},ISI_LTG{1,2},ISI_LTG{1,3}];
 
              x=size(analysis_ctrl{1, cell}(2).laser_peaks ,1);
-        %Plot the control and LTG post-stimulus times in a histogram of the
+        %Plot the control and LTG Interstimulus inteval times in a histogram of the
         %current cell and sweep. And add titels, legends etc.
         histogram(comb_ISI_ctrl,0:window,'FaceColor','k')
         hold on
@@ -91,7 +93,7 @@ for cell=[which_cells];
         alpha(.3);
         c=c+1;
         
-        
+        %Plot the Interstimulus time intevals per laser pulse.
         subplot(nr_sw,2,c);
         plot(c_ISI_ctrl,'k*')
         hold on
