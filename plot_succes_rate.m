17bb637e6ffd0ec1d5a6a48d9ee629bdb361bdc9
pjeterful <pieterstobbe@hotmail.com>
2016-07-20 16:49:40 +0200

    addapted some more scrips to be competable with electrical stimulation measurements

--- plot_succes_rate.m
+++ plot_succes_rate.m
@@ -9,6 +9,7 @@ nr_cells=size(analysis_ctrl,2);
 
 % Make a subplot for each cell
 for cell=1:nr_cells;
+    if isfield(analysis_ctrl{1, cell},'laser_intensity')==1
     subplot(ceil(sqrt(nr_cells)), ceil(sqrt(nr_cells)),cell);
     hold on
     % Set x and y values of the current cell for ctrl and LTG conditions
@@ -25,4 +26,26 @@ for cell=1:nr_cells;
     legend('control','LTG');
     title(['neuron: ' num2str(cell)]);
     axis([las_LTG(1), las_LTG(size(las_LTG,1)), 0, 1]);
+    end
+    
+   for cell=1:nr_cells;
+    if isfield(analysis_ctrl{1, cell},'elec_intensity')==1
+    subplot(ceil(sqrt(nr_cells)), ceil(sqrt(nr_cells)),cell);
+    hold on
+    % Set x and y values of the current cell for ctrl and LTG conditions
+    las_ctrl=analysis_ctrl{1,cell}.elec_intensity;
+    mn_ctrl=analysis_ctrl{1,cell}.mean_succes_rate;
+    las_LTG=analysis_LTG{1,cell}.elec_intensity;
+    mn_LTG=analysis_LTG{1,cell}.mean_succes_rate;
+    % Plot the current cell and create correpsponding axis labels, 
+    % axis values,  legend, title and set the axis
+    plot(las_ctrl ,mn_ctrl,'k');
+    plot(las_LTG, mn_LTG, 'r');
+    xlabel('stimulus intensity (%)');
+    ylabel('succes rate');
+    legend('control','LTG');
+    title(['neuron: ' num2str(cell)]);
+    axis([las_LTG(1), las_LTG(size(las_LTG,1)), 0, 1]);
+    end 
+end
 end
